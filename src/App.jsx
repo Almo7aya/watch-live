70faf5b11c01be6d8b58a9989c8a023317c95727
Ali Almohaya <alialmohaya@gmail.com>
2023-10-04 15:17:12 +0300

    chore: add files

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import VideoJS from './components/videojs';
+
+const channels = [
+  {
+    name: 'Bein Sports 1',
+    src: 'http://ir14.footballii.ir/hls2/bein1.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+  {
+    name: 'Bein Sports 2',
+    src: 'http://ir14.footballii.ir/hls2/bein2.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+  {
+    name: 'Bein Sports 3',
+    src: 'http://ir14.footballii.ir/hls2/bein3.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+  {
+    name: 'Bein Sports 4',
+    src: 'http://af.ayassport.ir/hls2/bein4.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+  {
+    name: 'SCC 1',
+    src: 'http://af.ayassport.ir/hls2/ssc1.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+  {
+    name: 'SCC 2',
+    src: 'http://af.ayassport.ir/hls2/ssc2.m3u8',
+    image: 'https://www.beinsports.com/au/static/img/logo.png',
+  },
+
+];
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      {channels.map((channel) => (
+        <div className='flex justify-center flex-col' key={channel.name}>
+          <h2 className="text-xl text-center my-6">{channel.name}</h2>
+          <VideoJS src={channel.src} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
